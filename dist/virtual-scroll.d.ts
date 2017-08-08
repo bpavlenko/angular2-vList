2783bcdf1dd5359183bc703d6cecf92c402e8475
pavlenko.bohd <pavlenko.bohd@gmail.com>
2017-08-08 15:00:54 +0300

    added dist

---
+++ dist/virtual-scroll.d.ts
@@ -0,0 +1,36 @@
+import { ElementRef, EventEmitter, OnChanges, OnDestroy, OnInit, Renderer, SimpleChanges } from '@angular/core';
+export interface ChangeEvent {
+    start?: number;
+    end?: number;
+}
+export declare class VirtualScrollComponent implements OnInit, OnDestroy, OnChanges {
+    private element;
+    private renderer;
+    items: any[];
+    scrollbarWidth: number;
+    scrollbarHeight: number;
+    childWidth: number;
+    childHeight: number;
+    update: EventEmitter<any[]>;
+    change: EventEmitter<ChangeEvent>;
+    start: EventEmitter<ChangeEvent>;
+    end: EventEmitter<ChangeEvent>;
+    contentElementRef: ElementRef;
+    onScrollListener: Function;
+    topPadding: number;
+    scrollHeight: number;
+    previousStart: number;
+    previousEnd: number;
+    startupLoop: boolean;
+    constructor(element: ElementRef, renderer: Renderer);
+    ngOnInit(): void;
+    ngOnChanges(changes: SimpleChanges): void;
+    ngOnDestroy(): void;
+    refresh(): void;
+    scrollInto(item: any): void;
+    private countItemsPerRow();
+    private calculateDimensions();
+    private calculateItems();
+}
+export declare class VirtualScrollModule {
+}
